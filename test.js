676a3073be74d644720612c2f39133bf0e17b288
André Carvalho <andre.daniel.carvalho@gmail.com>
2021-02-24 19:28:45 +0100

    * Dump data

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ const path = require('path')
 const resolve = path.resolve
 const tf = require('@tensorflow/tfjs-node')
 const sentenceModel = require('@tensorflow-models/universal-sentence-encoder')
-const toxicity = require('@tensorflow-models/toxicity')
 
 const chunk = (arr, size) =>
     Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
@@ -24,33 +23,10 @@ function encode(data) {
     })
 }
 
-
-function isToxic(text) {
-    return new Promise((res, rej) => {
-        toxicity.load(0.75)
-            .then((model) => {
-                model.classify(text)
-                    .then((results) => {
-                        console.log()
-                    })
-                    .catch((error) => rej(error))
-            })
-            .catch((error) => rej(error))
-    })
-}
-
 const items = JSON.parse(fs.readFileSync('./training/test.json'))
 
 encode(items.map(e => e.text.toLowerCase()))
     .then((data) => {
-        isToxic("Fuck you")
-            .then(() => {})
-            .catch((error) => console.log(error))
-
-        isToxic("Hello")
-            .then(() => {})
-            .catch((error) => console.log(error))
-
         tf.loadLayersModel(`file:///${resolve('./')}/output/model/model.json`)
             .then((model) => {
                 const tensor = model.predict(data)
